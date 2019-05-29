23e8e69252fdd21aae6501c05e69ea3581a15e93
grtumanyan <grtumanyan@gmail.com>
2019-05-29 17:30:54 +0400

    [FEAT]: Added swagger as documentation for API endpoints

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -5,23 +5,156 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Company;
 
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="api_token",
+ *         type="apiKey",
+ *         name="api_token",
+ *         in="query"
+ *     ),
+ * 		@SWG\Definition(
+ * 			definition="Company",
+ * 			required={"name", "parent"},
+ * 			@SWG\Property(property="id", type="string", description="UUID"),
+ * 			@SWG\Property(property="name", type="string"),
+ * 			@SWG\Property(property="parent", type="string"),
+ * 		),
+ * 		@SWG\Definition(
+ * 			definition="Station",
+ * 			required={"name", "latitude", "longitude", "company"},
+ * 			@SWG\Property(property="id", type="string", description="UUID"),
+ * 			@SWG\Property(property="name", type="string"),
+ * 			@SWG\Property(property="latitude", type="string"),
+ * 			@SWG\Property(property="longitude", type="string"),
+ * 			@SWG\Property(property="company", type="string"),
+ * 		),
+ *     host="localhost/devolon/public",
+ *     basePath="/api",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="API",
+ *         description="Api",
+ *         termsOfService="",
+ *         @SWG\License(
+ *             name="MIT"
+ *         )
+ *     )
+ * )
+ */
+
 class CompanyController extends Controller
 {
+
+    /**
+     * @SWG\Get(
+     *      path="/companies",
+     *      operationId="getCompanies",
+     *      tags={"Companies"},
+     *      summary="Get list of companies",
+     *      description="Returns list of companies",
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(ref="#/definitions/Company"),
+     *       ),
+     *       @SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       }
+     *     )
+     * Returns list of companies
+     */
     public function index()
     {
         return Company::all();
     }
 
+    /**
+     * @SWG\Get(
+     * 		path="/companies/{id}",
+     * 		tags={"Companies"},
+     * 		operationId="getCompany",
+     * 		summary="Fetch company details",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(ref="#/definitions/Company"),
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function show(Company $company)
     {
         return $company;
     }
 
+    /**
+     * @SWG\Get(
+     * 		path="/companies/tree/{id}",
+     * 		tags={"Companies"},
+     * 		operationId="getTree",
+     * 		summary="Get stations tree by companyId",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(ref="#/definitions/Company"),
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function tree(Company $company)
     {
-        return response()->json($company->allChildren()->get(), 200);
+        return response()->json($company->allChildren()->get());
     }
 
+    /**
+     *
+     * @SWG\Post(
+     * 		path="/companies",
+     * 		tags={"Companies"},
+     * 		operationId="createCompany",
+     * 		summary="Create new company entry",
+     * 		@SWG\Parameter(
+     * 			name="company",
+     * 			in="body",
+     * 			required=true,
+     *          @SWG\Schema(ref="#/definitions/Company"),
+     *		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function store(Request $request)
     {
         $company = Company::create($request->all());
@@ -29,6 +162,37 @@ class CompanyController extends Controller
         return response()->json($company, 201);
     }
 
+    /**
+     *
+     * 	@SWG\Put(
+     * 		path="/companies/{id}",
+     * 		tags={"Companies"},
+     * 		operationId="updateCompany",
+     * 		summary="Update company entry",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="company",
+     * 			in="body",
+     * 			required=true,
+     *          @SWG\Schema(ref="#/definitions/Company"),
+     *		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function update(Request $request, Company $company)
     {
         $company->update($request->all());
@@ -36,6 +200,32 @@ class CompanyController extends Controller
         return response()->json($company, 200);
     }
 
+    /**
+     *
+     * 	@SWG\Delete(
+     * 		path="/companies/{id}",
+     * 		tags={"Companies"},
+     * 		operationId="deleteCompany",
+     * 		summary="Remove company entry",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="success",
+     * 		),
+     * 		@SWG\Response(
+     * 			response="default",
+     * 			description="error",
+     * 			@SWG\Schema(ref="#/definitions/Company"),
+     * 		),
+     * 	)
+     *
+     */
     public function delete(Company $company)
     {
         $company->delete();
--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -16,21 +16,125 @@ class StationController extends Controller
         $this->stationService = $stationService;
     }
 
+    /**
+     * @SWG\Get(
+     *      path="/stations",
+     *      operationId="getStations",
+     *      tags={"Stations"},
+     *      summary="Get list of stations",
+     *      description="Returns list of stations",
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(ref="#/definitions/Station"),
+     *       ),
+     *       @SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       }
+     *     )
+     * Returns list of stations
+     */
     public function index()
     {
         return Station::all();
     }
 
+    /**
+     * @SWG\Get(
+     * 		path="/radius",
+     * 		tags={"Stations"},
+     * 		operationId="getByRadius",
+     * 		summary="Get stations by given place and radius",
+     * 		@SWG\Parameter(
+     * 			name="latitude",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="longitude",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="radius",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(ref="#/definitions/Station"),
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function radius(Request $request)
     {
         return $this->stationService->getByRadius($request);
     }
 
+    /**
+     * @SWG\Get(
+     * 		path="/stations/{id}",
+     * 		tags={"Stations"},
+     * 		operationId="getStation",
+     * 		summary="Fetch station details",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(ref="#/definitions/Station"),
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function show(Station $station)
     {
         return $station;
     }
 
+    /**
+     *
+     * @SWG\Post(
+     * 		path="/stations",
+     * 		tags={"Stations"},
+     * 		operationId="createStation",
+     * 		summary="Create new station entry",
+     * 		@SWG\Parameter(
+     * 			name="station",
+     * 			in="body",
+     * 			required=true,
+     *          @SWG\Schema(ref="#/definitions/Station"),
+     *		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function store(Request $request)
     {
         $station = Station::create($request->all());
@@ -38,6 +142,38 @@ class StationController extends Controller
         return response()->json($station, 201);
     }
 
+
+    /**
+     *
+     * 	@SWG\Put(
+     * 		path="/stations/{id}",
+     * 		tags={"Stations"},
+     * 		operationId="updateStation",
+     * 		summary="Update station entry",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="station",
+     * 			in="body",
+     * 			required=true,
+     *          @SWG\Schema(ref="#/definitions/Station"),
+     *		),
+     * 		@SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     * 		@SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_token": {}}
+     *       },
+     * 	)
+     *
+     */
     public function update(Request $request, Station $station)
     {
         $station->update($request->all());
@@ -45,6 +181,32 @@ class StationController extends Controller
         return response()->json($station, 200);
     }
 
+    /**
+     *
+     * 	@SWG\Delete(
+     * 		path="/stations/{id}",
+     * 		tags={"Stations"},
+     * 		operationId="deleteStation",
+     * 		summary="Remove station entry",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="string",
+     * 			description="UUID",
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="success",
+     * 		),
+     * 		@SWG\Response(
+     * 			response="default",
+     * 			description="error",
+     * 			@SWG\Schema(ref="#/definitions/Station"),
+     * 		),
+     * 	)
+     *
+     */
     public function delete(Station $station)
     {
         $station->delete();
